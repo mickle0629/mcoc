dcd9f88dc1c2b3a497009806b0c0476ed3b22d5c
Tricia Cebotari <pcebotari24@my.whitworth.edu>
2024-04-04 10:43:17 -0700

    new pages

---
+++ src/app/inventory/page.tsx
@@ -0,0 +1,9 @@
+import SearchShoes from "../lib/components/inventory-landing-page"
+
+export default function BrowseShoes() {
+    return (
+        <main className="flex flex-col bg-white min-h-screen">
+            <SearchShoes />
+        </main>
+    )
+}

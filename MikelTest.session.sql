6eed64e4811468e7875edaff1a5d075103c329cf
sethmiller24 <sethmiller24@whitworth.edu>
2024-05-07 10:03:23 -0700

    another query

--- MikelTest.session.sql
+++ MikelTest.session.sql
@@ -1,2 +1,23 @@
-UPDATE available_shoe
-SET numSold = 0;
+SELECT ST.shoename, AvS.shoesize, AvS.inventory, AvS.numsold
+FROM shoetype AS ST, available_shoe AS AvS
+WHERE AvS.shoetype = 1 AND ST.shoetypeid = AvS.shoetype;
+
+SELECT ST.shoename, AvS.shoesize, AvS.inventory, AvS.numsold
+FROM shoetype AS ST, available_shoe AS AvS
+WHERE AvS.shoetype = 3 AND ST.shoetypeid = AvS.shoetype;
+
+SELECT ST.shoename, AvS.shoesize, AvS.inventory, AvS.numsold
+FROM shoetype AS ST, available_shoe AS AvS
+WHERE AvS.shoetype = 5 AND ST.shoetypeid = AvS.shoetype;
+
+SELECT ST.shoename, AvS.shoesize, AvS.inventory, AvS.numsold
+FROM shoetype AS ST, available_shoe AS AvS
+WHERE AvS.shoetype = 2 AND ST.shoetypeid = AvS.shoetype;
+
+SELECT ST.shoename, AvS.shoesize, AvS.inventory, AvS.numsold
+FROM shoetype AS ST, available_shoe AS AvS
+WHERE AvS.shoetype = 4 AND ST.shoetypeid = AvS.shoetype;
+
+SELECT ST.shoename, AvS.shoesize, AvS.inventory, AvS.numsold
+FROM shoetype AS ST, available_shoe AS AvS
+WHERE AvS.shoetype = 6 AND ST.shoetypeid = AvS.shoetype;

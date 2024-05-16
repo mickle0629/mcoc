2f0312da383ca04f82ce482883c927db811e50ee
Tricia Cebotari <pcebotari24@my.whitworth.edu>
2024-05-16 16:48:48 -0700

    homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
 /**
  * Starting page. Hosts the information entry component to get info from customers.
  */
+import Link from 'next/link'
 
 
 
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen bg-white">
+    <div className="w-auto content-center flex flex-col items-center p-10">
+      <button className="w-80 px-4 py-2 mb-2 bg-green-500 text-white rounded-full"><Link href="/parent-information">Add Parent</Link></button>
+      <button className="w-80 px-4 py-2 mb-2 bg-green-500 text-white rounded-full"><Link href="/kidInfo">Add Child</Link></button>
+      <button className="w-80 px-4 py-2 mb-2 bg-green-500 text-white rounded-full"><Link href="/browse">View Available Shoes</Link></button>
+    </div>
+
     {/* footer */}
     <div className="h-12 mb-3 p-1 border-t-2 border-gray-400">
-        <button  className=" flex justify-center px-10 py-2 mb-2 bg-green-500 text-white rounded-full" >Add Parent</button>
-      {/*TODO: has too much bottom padding/margin */}
       <p className="text-black text-center pt-3">Copyright 2024</p>
     </div>
   </main>

6e68a0ec0cf27ab6d47cea49a5d8b9ac14ce3a5d
Irene Racharla <iracharla24@my.whitworth.edu>
2024-05-17 02:03:39 -0700

    add conf to checkout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
       <Link href="/parent-information"><button className="mt-3 w-80 px-4 py-2 mb-2 bg-green-500 text-white rounded-full hover:bg-sky-700">Add Guardian</button></Link>
         <DisplayParent parentId = {idNumber}></DisplayParent>
       <Link href="/kidInfo"><button className="mt-3 w-80 px-4 py-2 mb-2 bg-green-500 text-white rounded-full hover:bg-sky-700">Add Child</button></Link>
-      <Link href=""><button className="mt-3 w-80 px-4 py-2 mb-2 bg-green-500 text-white rounded-full hover:bg-sky-700">Checkout</button></Link>
+      <Link href="/confirmation"><button className="mt-3 w-80 px-4 py-2 mb-2 bg-green-500 text-white rounded-full hover:bg-sky-700">Checkout</button></Link>
     </div>
 
     {/* footer */}

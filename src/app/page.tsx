8673396bfc494235ff5bd4d934df5ff8b3c2ba1c
Tricia Cebotari <pcebotari24@my.whitworth.edu>
2024-05-17 15:14:59 -0700

    more spell checks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
         <Link href="/admin"><button className="px-4 py-2 bg-green-500 text-white rounded-full hover:bg-sky-700 mt-10">Admin</button></Link>
       </div>
     <div className="w-auto content-center flex flex-col items-center p-10 h-screen">
-      <Link href="/parent-information"><button className="mt-3 w-80 px-4 py-2 mb-2 bg-green-500 text-white rounded-full hover:bg-sky-700">Add Gaurdian</button></Link>
+      <Link href="/parent-information"><button className="mt-3 w-80 px-4 py-2 mb-2 bg-green-500 text-white rounded-full hover:bg-sky-700">Add Guardian</button></Link>
         <DisplayParent parentId = {idNumber}></DisplayParent>
       <Link href={kidurl}><button onClick={reload} className="mt-3 w-80 px-4 py-2 mb-2 bg-green-500 text-white rounded-full hover:bg-sky-700">Add Child</button></Link>
       <Link href="/comfirmation"><button className="mt-3 w-80 px-4 py-2 mb-2 bg-green-500 text-white rounded-full hover:bg-sky-700">Submit</button></Link>

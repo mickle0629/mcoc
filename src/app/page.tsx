63c2f4e3a22177bee972c34a1728df54e9193f01
Tricia Cebotari <pcebotari24@my.whitworth.edu>
2024-05-17 15:18:04 -0700

    confirm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
       <Link href="/parent-information"><button className="mt-3 w-80 px-4 py-2 mb-2 bg-green-500 text-white rounded-full hover:bg-sky-700">Add Guardian</button></Link>
         <DisplayParent parentId = {idNumber}></DisplayParent>
       <Link href={kidurl}><button onClick={reload} className="mt-3 w-80 px-4 py-2 mb-2 bg-green-500 text-white rounded-full hover:bg-sky-700">Add Child</button></Link>
-      <Link href="/comfirmation"><button className="mt-3 w-80 px-4 py-2 mb-2 bg-green-500 text-white rounded-full hover:bg-sky-700">Submit</button></Link>
+      <Link href="/confirmation"><button className="mt-3 w-80 px-4 py-2 mb-2 bg-green-500 text-white rounded-full hover:bg-sky-700">Submit</button></Link>
     </div>
 
     {/* footer */}

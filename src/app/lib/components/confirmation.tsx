ea3dceb5d13d99f17c17a9b4f6cfa2436c9ff440
mickle0629 <mikeyd2k2@gmail.com>
2024-05-16 20:40:59 -0700

    confirmation page updates + remove unused regexps

--- src/app/lib/components/confirmation.tsx
+++ src/app/lib/components/confirmation.tsx
@@ -19,27 +19,24 @@ const ConfirmationForm = () => {
     <Formik initialValues={initialValues} onSubmit={handleSubmit}>
       {({ values }) => (
         <Form>
-          <div>
-          <span className="text-black text-lg">Confirmation and Disclaimer</span>
-          <br />
-            <label>
-              <Field type="checkbox" name="agree1" />
-              <span className="text-black ml-2">I understand that I will need to pick up my kids' shoes on [DATE OF EVENT] from [TIME OF EVENT] at [LOCATION].</span>
-            </label>
+          <div className='flex flex-col justify-center text-left gap-5 items-center mx-auto my-20 py-12'>
+            <span className="text-black text-5xl text-center pb-10">Confirmation and Disclaimer</span>
+            <div className='flex flex-col gap-3 text-lg'>
+              <label>
+                <Field type="checkbox" name="agree1" />
+                <span className="text-black ml-2">I understand that I will need to pick up my kids' shoes on [DATE OF EVENT] from [TIME OF EVENT] at [LOCATION].</span>
+              </label>
+              <label>
+                <Field type="checkbox" name="agree2" />
+                <span className="text-black ml-2">I understand that I need to bring my confirmation email (on phone or printed) along with my ID to pick up the shoes.</span>
+              </label>
+              <label>
+                <Field type="checkbox" name="agree3" />
+                <span className="text-black ml-2">I understand that inventory is limited and I may not get the size shoes I have registered for.</span>
+              </label>
+            </div>
+            <button type="submit" className="px-10 py-6 mt-16 bg-green-500 text-white text-2xl rounded-full text-nowrap grow-0 ">Place Order</button>
           </div>
-          <div>
-            <label>
-              <Field type="checkbox" name="agree2" />
-              <span className="text-black ml-2">I understand that I need to bring my confirmation email (on phone or printed) along with my ID to pick up the shoes.</span>
-            </label>
-          </div>
-          <div>
-            <label>
-              <Field type="checkbox" name="agree3" />
-              <span className="text-black ml-2">I understand that inventory is limited and I may not get the size shoes I have registered for.</span>
-            </label>
-          </div>
-          <button type="submit" className="max-w-32 px-2 py-1 mb-2 bg-green-500 text-white rounded-full text-nowrap grow-0">Place Order</button>
         </Form>
       )}
     </Formik>
